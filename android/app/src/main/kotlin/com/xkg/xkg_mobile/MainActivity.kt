20eb144ff2ee9ed3d72536dcc615249c31be0c0d
griptoad <griptoad@github.com>
2026-03-16 11:41:42 -0700

    Fix MainActivity

--- android/app/src/main/kotlin/com/xkg/xkg_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/xkg/xkg_mobile/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.xkg.xkg_mobile
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
